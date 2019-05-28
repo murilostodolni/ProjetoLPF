8eb221cfe48cb01221439f42d4cf7d20bd5a6bc4
Pedro Cunha <pvcunha@zoho.com>
2019-05-28 19:26:17 -0300

    checa linha e update2

--- out/production/ProjetoLPF/ProjetoLPF.js
+++ out/production/ProjetoLPF/ProjetoLPF.js
@@ -3,18 +3,23 @@ if (typeof kotlin === 'undefined') {
 }
 var ProjetoLPF = function (_, Kotlin) {
   'use strict';
+  var println = Kotlin.kotlin.io.println_s8jyv4$;
   var throwCCE = Kotlin.throwCCE;
   var ensureNotNull = Kotlin.ensureNotNull;
-  var equals = Kotlin.equals;
-  var println = Kotlin.kotlin.io.println_s8jyv4$;
-  var equals_0 = Kotlin.kotlin.text.equals_igcy3c$;
   var toInt = Kotlin.kotlin.text.toInt_pdl1vz$;
+  var equals = Kotlin.equals;
+  var equals_0 = Kotlin.kotlin.text.equals_igcy3c$;
   var asList = Kotlin.org.w3c.dom.asList_kt9thq$;
   function clicaTeste(id) {
-    var tmp$;
+    var tmp$, tmp$_0;
+    println('2');
     var e = Kotlin.isType(tmp$ = document.getElementById(id), HTMLTableCellElement) ? tmp$ : throwCCE();
-    if (equals(ensureNotNull(e.firstElementChild).className, 'hashtag'))
-      e.innerHTML = '<img class="x" src="img/x.jpg"/>';
+    var rod = Kotlin.isType(tmp$_0 = document.getElementById('rod'), HTMLElement) ? tmp$_0 : throwCCE();
+    var valor = toInt(ensureNotNull(rod.textContent));
+    var img = valor % 2 === 0 ? '<img class="x" src="img/x.jpg"/>' : '<img class="x" src="img/circulo.jpg"/>';
+    if (equals(ensureNotNull(e.firstElementChild).className, 'branco'))
+      e.innerHTML = img;
+    attRodada(rod, valor);
     println(checaLinhas());
   }
   function testeCheca() {
@@ -29,8 +34,9 @@ var ProjetoLPF = function (_, Kotlin) {
       var e = Kotlin.isType(tmp$ = document.getElementById(id), HTMLTableCellElement) ? tmp$ : throwCCE();
       var rod = Kotlin.isType(tmp$_0 = document.getElementById('rod'), HTMLElement) ? tmp$_0 : throwCCE();
       var valor = toInt(ensureNotNull(rod.textContent));
-      jogada(e, valor);
+      jogada(e.firstElementChild, valor);
       attRodada(rod, valor);
+      println(checaLinhas());
       if (checaVitoria())
         if (valor % 2 === 0)
           fim('x');
@@ -56,42 +62,37 @@ var ProjetoLPF = function (_, Kotlin) {
     reinicia();
   }
   function jogada(elem, rodada) {
-    if (equals_0(elem.textContent, '#'))
+    if (equals_0(elem != null ? elem.className : null, 'branco'))
       if (rodada % 2 === 0)
-        elem.innerHTML = 'x';
+        elem != null ? (elem.innerHTML = '<img class="x" src="img/x.jpg"/>') : null;
       else
-        elem.innerHTML = 'o';
+        elem != null ? (elem.innerHTML = '<img class="x" src="img/circulo.jpg"/>') : null;
   }
   function attRodada(rod, valor) {
     rod.innerHTML = (valor + 1 | 0).toString();
   }
   function tabuleiroCheio() {
-    var tmp$;
     var l = document.getElementsByTagName('td');
-    var tmp$_0;
-    tmp$_0 = asList(l).iterator();
-    while (tmp$_0.hasNext()) {
-      var element = tmp$_0.next();
-      if (equals_0(element.textContent, '#'))
-        return false;
-    }
+    var tmp$;
     tmp$ = asList(l).iterator();
     while (tmp$.hasNext()) {
-      var e = tmp$.next();
-      println(e.textContent);
+      var element = tmp$.next();
+      var tmp$_0;
+      if (equals_0((tmp$_0 = element.firstElementChild) != null ? tmp$_0.className : null, 'branco'))
+        return false;
     }
     return true;
   }
   function checaLinhas$linha1Aux(closure$l) {
     return function () {
       var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5, tmp$_6, tmp$_7, tmp$_8, tmp$_9, tmp$_10, tmp$_11, tmp$_12;
-      var tmp$_13 = !equals_0((tmp$_0 = (tmp$ = closure$l[0]) != null ? tmp$.firstElementChild : null) != null ? tmp$_0.className : null, 'hashtag');
+      var tmp$_13 = !equals_0((tmp$_0 = (tmp$ = closure$l[0]) != null ? tmp$.firstElementChild : null) != null ? tmp$_0.className : null, 'branco');
       if (!tmp$_13) {
-        tmp$_13 = !equals_0((tmp$_2 = (tmp$_1 = closure$l[1]) != null ? tmp$_1.firstElementChild : null) != null ? tmp$_2.className : null, 'hashtag');
+        tmp$_13 = !equals_0((tmp$_2 = (tmp$_1 = closure$l[1]) != null ? tmp$_1.firstElementChild : null) != null ? tmp$_2.className : null, 'branco');
       }
       var tmp$_14 = tmp$_13;
       if (!tmp$_14) {
-        tmp$_14 = !equals_0((tmp$_4 = (tmp$_3 = closure$l[2]) != null ? tmp$_3.firstElementChild : null) != null ? tmp$_4.className : null, 'hashtag');
+        tmp$_14 = !equals_0((tmp$_4 = (tmp$_3 = closure$l[2]) != null ? tmp$_3.firstElementChild : null) != null ? tmp$_4.className : null, 'branco');
       }
       if (tmp$_14) {
         return equals_0((tmp$_6 = (tmp$_5 = closure$l[0]) != null ? tmp$_5.firstElementChild : null) != null ? tmp$_6.className : null, (tmp$_8 = (tmp$_7 = closure$l[1]) != null ? tmp$_7.firstElementChild : null) != null ? tmp$_8.className : null) ? equals_0((tmp$_10 = (tmp$_9 = closure$l[0]) != null ? tmp$_9.firstElementChild : null) != null ? tmp$_10.className : null, (tmp$_12 = (tmp$_11 = closure$l[2]) != null ? tmp$_11.firstElementChild : null) != null ? tmp$_12.className : null) : false;
@@ -102,17 +103,17 @@ var ProjetoLPF = function (_, Kotlin) {
   }
   function checaLinhas$linha2Aux(closure$l) {
     return function () {
-      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
-      var tmp$_6 = !equals_0((tmp$ = closure$l[3]) != null ? tmp$.textContent : null, '#');
-      if (!tmp$_6) {
-        tmp$_6 = !equals_0((tmp$_0 = closure$l[4]) != null ? tmp$_0.textContent : null, '#');
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5, tmp$_6, tmp$_7, tmp$_8, tmp$_9, tmp$_10, tmp$_11, tmp$_12;
+      var tmp$_13 = !equals_0((tmp$_0 = (tmp$ = closure$l[3]) != null ? tmp$.firstElementChild : null) != null ? tmp$_0.className : null, 'branco');
+      if (!tmp$_13) {
+        tmp$_13 = !equals_0((tmp$_2 = (tmp$_1 = closure$l[4]) != null ? tmp$_1.firstElementChild : null) != null ? tmp$_2.className : null, 'branco');
       }
-      var tmp$_7 = tmp$_6;
-      if (!tmp$_7) {
-        tmp$_7 = !equals_0((tmp$_1 = closure$l[5]) != null ? tmp$_1.textContent : null, '#');
+      var tmp$_14 = tmp$_13;
+      if (!tmp$_14) {
+        tmp$_14 = !equals_0((tmp$_4 = (tmp$_3 = closure$l[5]) != null ? tmp$_3.firstElementChild : null) != null ? tmp$_4.className : null, 'branco');
       }
-      if (tmp$_7) {
-        return equals_0((tmp$_2 = closure$l[3]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[4]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[3]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[5]) != null ? tmp$_5.textContent : null) : false;
+      if (tmp$_14) {
+        return equals_0((tmp$_6 = (tmp$_5 = closure$l[3]) != null ? tmp$_5.firstElementChild : null) != null ? tmp$_6.className : null, (tmp$_8 = (tmp$_7 = closure$l[4]) != null ? tmp$_7.firstElementChild : null) != null ? tmp$_8.className : null) ? equals_0((tmp$_10 = (tmp$_9 = closure$l[3]) != null ? tmp$_9.firstElementChild : null) != null ? tmp$_10.className : null, (tmp$_12 = (tmp$_11 = closure$l[5]) != null ? tmp$_11.firstElementChild : null) != null ? tmp$_12.className : null) : false;
       }
        else
         return false;
@@ -120,17 +121,17 @@ var ProjetoLPF = function (_, Kotlin) {
   }
   function checaLinhas$linha3Aux(closure$l) {
     return function () {
-      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
-      var tmp$_6 = !equals_0((tmp$ = closure$l[6]) != null ? tmp$.textContent : null, '#');
-      if (!tmp$_6) {
-        tmp$_6 = !equals_0((tmp$_0 = closure$l[7]) != null ? tmp$_0.textContent : null, '#');
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5, tmp$_6, tmp$_7, tmp$_8, tmp$_9, tmp$_10, tmp$_11, tmp$_12;
+      var tmp$_13 = !equals_0((tmp$_0 = (tmp$ = closure$l[6]) != null ? tmp$.firstElementChild : null) != null ? tmp$_0.className : null, 'branco');
+      if (!tmp$_13) {
+        tmp$_13 = !equals_0((tmp$_2 = (tmp$_1 = closure$l[7]) != null ? tmp$_1.firstElementChild : null) != null ? tmp$_2.className : null, 'branco');
       }
-      var tmp$_7 = tmp$_6;
-      if (!tmp$_7) {
-        tmp$_7 = !equals_0((tmp$_1 = closure$l[8]) != null ? tmp$_1.textContent : null, '#');
+      var tmp$_14 = tmp$_13;
+      if (!tmp$_14) {
+        tmp$_14 = !equals_0((tmp$_4 = (tmp$_3 = closure$l[8]) != null ? tmp$_3.firstElementChild : null) != null ? tmp$_4.className : null, 'branco');
       }
-      if (tmp$_7) {
-        return equals_0((tmp$_2 = closure$l[6]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[7]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[6]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[8]) != null ? tmp$_5.textContent : null) : false;
+      if (tmp$_14) {
+        return equals_0((tmp$_6 = (tmp$_5 = closure$l[6]) != null ? tmp$_5.firstElementChild : null) != null ? tmp$_6.className : null, (tmp$_8 = (tmp$_7 = closure$l[7]) != null ? tmp$_7.firstElementChild : null) != null ? tmp$_8.className : null) ? equals_0((tmp$_10 = (tmp$_9 = closure$l[6]) != null ? tmp$_9.firstElementChild : null) != null ? tmp$_10.className : null, (tmp$_12 = (tmp$_11 = closure$l[8]) != null ? tmp$_11.firstElementChild : null) != null ? tmp$_12.className : null) : false;
       }
        else
         return false;
@@ -141,7 +142,7 @@ var ProjetoLPF = function (_, Kotlin) {
     var linha1Aux = checaLinhas$linha1Aux(l);
     var linha2Aux = checaLinhas$linha2Aux(l);
     var linha3Aux = checaLinhas$linha3Aux(l);
-    return linha1Aux();
+    return linha1Aux() || linha2Aux() || linha3Aux();
   }
   function checaColuna$coluna1Aux(closure$l) {
     return function () {
@@ -247,19 +248,19 @@ var ProjetoLPF = function (_, Kotlin) {
     return diagonal1Aux() || diagonal2Aux();
   }
   function checa() {
-    return checaLinhas() || checaColuna() || checaDiagonal() || tabuleiroCheio();
+    return checaLinhas();
   }
   function checaVitoria() {
     return checaLinhas() || checaColuna() || checaDiagonal();
   }
   function main() {
   }
-  _.clicaTeste = clicaTeste;
+  _.update2 = clicaTeste;
   _.testeCheca = testeCheca;
   _.update = update;
   _.reinicia = reinicia;
   _.fim_61zpoe$ = fim;
-  _.jogada_906ova$ = jogada;
+  _.jogada_gpzq8i$ = jogada;
   _.attRodada_906ova$ = attRodada;
   _.tabuleiroCheio = tabuleiroCheio;
   _.checaLinhas = checaLinhas;
--- src/Velha.kt
+++ src/Velha.kt
@@ -2,21 +2,24 @@ import org.w3c.dom.*
 import org.w3c.dom.events.MouseEvent
 import kotlin.browser.*
 
-@JsName("clicaTeste")
+@JsName("update2")
 fun clicaTeste(id: String){
+    println("2")
+
     val e = document.getElementById(id) as HTMLTableCellElement
-    if(e.firstElementChild!!.className.equals("hashtag")) e.innerHTML = "<img class=\"x\" src=\"img/x.jpg\"/>"
+    val rod = document.getElementById("rod") as HTMLElement
+    val valor: Int = rod.textContent!!.toInt()
+
+    val img = if(valor%2==0) "<img class=\"x\" src=\"img/x.jpg\"/>" else "<img class=\"x\" src=\"img/circulo.jpg\"/>"
+
+    if(e.firstElementChild!!.className.equals("branco")) e.innerHTML = img
+
+    attRodada(rod, valor)//atualiza o valor da rodada
 
     println(checaLinhas())
 }
 
-fun testeCheca(){
-    val l = document.getElementsByTagName("td")
-    println("pula")
-    println(l.get(0)?.firstElementChild?.className.equals("x"))
-}
-
-@JsName("update")
+/*@JsName("update")
 fun update(id: String): Unit{
     if(!checa()) {
         val e = document.getElementById(id) as HTMLTableCellElement
@@ -24,14 +27,16 @@ fun update(id: String): Unit{
         val valor: Int = rod.textContent!!.toInt()
 
 
-        jogada(e, valor)
+        jogada(e.firstElementChild, valor)
         attRodada(rod, valor)//atualiza o valor da rodada
+
+        println(checaLinhas())
 
         if(checaVitoria()) if(valor%2==0) fim("x") else fim("o")
     } else
-        if(tabuleiroCheio()) window.alert("Empate!")
+       if(tabuleiroCheio()) window.alert("Empate!")
 
-}
+}*/
 
 @JsName("reinicia")
 fun reinicia(): Unit{
@@ -47,13 +52,6 @@ fun fim(c: String): Unit {
     reinicia()
 }
 
-fun jogada(elem: HTMLElement, rodada: Int): Unit{ //faz um movimento no html ao clicar numa celula vazia
-    if (elem.textContent.equals("#"))
-        if (rodada % 2 == 0)
-            elem.innerHTML = "x"
-        else
-            elem.innerHTML = "o"
-}
 
 fun attRodada(rod: HTMLElement, valor: Int): Unit{
     rod.innerHTML= (valor+1).toString()
@@ -62,10 +60,8 @@ fun attRodada(rod: HTMLElement, valor: Int): Unit{
 fun tabuleiroCheio(): Boolean{
     val l = document.getElementsByTagName("td")
     l.asList().forEach { obj ->
-        if(obj.textContent.equals("#")) return false
+        if(obj.firstElementChild?.className.equals("branco")) return false
     }
-    for(e in l.asList())
-        println(e.textContent)
     return true
 }
 
@@ -73,28 +69,30 @@ fun checaLinhas(): Boolean{
     val l = document.getElementsByTagName("td")
 
     fun linha1Aux(): Boolean { //checa linha 1
-        if(!l.get(0)?.firstElementChild?.className.equals("hashtag") || !l.get(1)?.firstElementChild?.className.equals("hashtag")
-                || !l.get(2)?.firstElementChild?.className.equals("hashtag"))
+        if(!l.get(0)?.firstElementChild?.className.equals("branco") || !l.get(1)?.firstElementChild?.className.equals("branco")
+                || !l.get(2)?.firstElementChild?.className.equals("branco"))
             return (l.get(0)?.firstElementChild?.className.equals(l.get(1)?.firstElementChild?.className) && l.get(0)?.firstElementChild?.className.equals(l.get(2)?.firstElementChild?.className))
         else
             return false
     }
 
     fun linha2Aux(): Boolean { //checa linha 2
-        if(!l.get(3)?.textContent.equals("#") || !l.get(4)?.textContent.equals("#") || !l.get(5)?.textContent.equals("#"))
-            return (l.get(3)?.textContent.equals(l.get(4)?.textContent) && l.get(3)?.textContent.equals(l.get(5)?.textContent))
+        if(!l.get(3)?.firstElementChild?.className.equals("branco") || !l.get(4)?.firstElementChild?.className.equals("branco")
+                || !l.get(5)?.firstElementChild?.className.equals("branco"))
+            return (l.get(3)?.firstElementChild?.className.equals(l.get(4)?.firstElementChild?.className) && l.get(3)?.firstElementChild?.className.equals(l.get(5)?.firstElementChild?.className))
         else
             return false
     }
 
     fun linha3Aux(): Boolean { //checa linha 3
-        if(!l.get(6)?.textContent.equals("#") || !l.get(7)?.textContent.equals("#") || !l.get(8)?.textContent.equals("#"))
-            return (l.get(6)?.textContent.equals(l.get(7)?.textContent) && l.get(6)?.textContent.equals(l.get(8)?.textContent))
+        if(!l.get(6)?.firstElementChild?.className.equals("branco") || !l.get(7)?.firstElementChild?.className.equals("branco")
+                || !l.get(8)?.firstElementChild?.className.equals("branco"))
+            return (l.get(6)?.firstElementChild?.className.equals(l.get(7)?.firstElementChild?.className) && l.get(6)?.firstElementChild?.className.equals(l.get(8)?.firstElementChild?.className))
         else
             return false
     }
 
-    return linha1Aux()
+    return linha1Aux() || linha2Aux() || linha3Aux()
 }
 
 fun checaColuna(): Boolean{
@@ -144,7 +142,7 @@ fun checaDiagonal(): Boolean{
     return diagonal1Aux() || diagonal2Aux()
 }
 
-fun checa(): Boolean = checaLinhas() || checaColuna() || checaDiagonal() || tabuleiroCheio()
+fun checa(): Boolean = checaLinhas()
 
 fun checaVitoria() = checaLinhas() || checaColuna() || checaDiagonal()
 
