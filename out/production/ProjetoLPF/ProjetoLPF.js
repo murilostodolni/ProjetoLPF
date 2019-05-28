958899f0f5a49d07c80f256ade43b97293fcc992
Pedro Cunha <pvcunha@zoho.com>
2019-05-28 18:52:45 -0300

    att jogo da velha com img

--- out/production/ProjetoLPF/ProjetoLPF.js
+++ out/production/ProjetoLPF/ProjetoLPF.js
@@ -5,37 +5,268 @@ var ProjetoLPF = function (_, Kotlin) {
   'use strict';
   var throwCCE = Kotlin.throwCCE;
   var ensureNotNull = Kotlin.ensureNotNull;
+  var equals = Kotlin.equals;
+  var println = Kotlin.kotlin.io.println_s8jyv4$;
+  var equals_0 = Kotlin.kotlin.text.equals_igcy3c$;
   var toInt = Kotlin.kotlin.text.toInt_pdl1vz$;
-  function mostra() {
-    var a = get('oi');
-    a.innerHTML = a.innerHTML + 'pegou';
-  }
-  function get(nome) {
+  var asList = Kotlin.org.w3c.dom.asList_kt9thq$;
+  function clicaTeste(id) {
     var tmp$;
-    var e = document.getElementById(nome);
-    return Kotlin.isType(tmp$ = e, HTMLElement) ? tmp$ : throwCCE();
+    var e = Kotlin.isType(tmp$ = document.getElementById(id), HTMLTableCellElement) ? tmp$ : throwCCE();
+    if (equals(ensureNotNull(e.firstElementChild).className, 'hashtag'))
+      e.innerHTML = '<img class="x" src="img/x.jpg"/>';
+    println(checaLinhas());
   }
-  function jogada(pos) {
-    var tmp$, tmp$_0, tmp$_1, tmp$_2;
-    var canv = Kotlin.isType(tmp$ = document.getElementById(pos), HTMLCanvasElement) ? tmp$ : throwCCE();
-    var n = document.getElementById('numJogadas');
-    var valor = n != null ? toInt(ensureNotNull(n.textContent)) : 0;
-    var img = Kotlin.isType(tmp$_0 = document.getElementById('circulo'), HTMLImageElement) ? tmp$_0 : throwCCE();
-    if (valor % 2 === 0) {
-      img = Kotlin.isType(tmp$_1 = document.getElementById('x'), HTMLImageElement) ? tmp$_1 : throwCCE();
+  function testeCheca() {
+    var tmp$, tmp$_0;
+    var l = document.getElementsByTagName('td');
+    println('pula');
+    println(equals_0((tmp$_0 = (tmp$ = l[0]) != null ? tmp$.firstElementChild : null) != null ? tmp$_0.className : null, 'x'));
+  }
+  function update(id) {
+    var tmp$, tmp$_0;
+    if (!checa()) {
+      var e = Kotlin.isType(tmp$ = document.getElementById(id), HTMLTableCellElement) ? tmp$ : throwCCE();
+      var rod = Kotlin.isType(tmp$_0 = document.getElementById('rod'), HTMLElement) ? tmp$_0 : throwCCE();
+      var valor = toInt(ensureNotNull(rod.textContent));
+      jogada(e, valor);
+      attRodada(rod, valor);
+      if (checaVitoria())
+        if (valor % 2 === 0)
+          fim('x');
+        else
+          fim('o');
     }
-    var context = Kotlin.isType(tmp$_2 = canv.getContext('2d'), CanvasRenderingContext2D) ? tmp$_2 : throwCCE();
-    context.drawImage(img, 0.0, 0.0, 100.0, 100.0);
-    n != null ? (n.innerHTML = (valor + 1 | 0).toString()) : null;
+     else if (tabuleiroCheio())
+      window.alert('Empate!');
+  }
+  function reinicia() {
+    var tmp$;
+    var e = document.getElementsByTagName('td');
+    var tmp$_0;
+    tmp$_0 = asList(e).iterator();
+    while (tmp$_0.hasNext()) {
+      var element = tmp$_0.next();
+      element.innerHTML = '#';
+    }
+    (tmp$ = document.getElementById('rod')) != null ? (tmp$.innerHTML = (0).toString()) : null;
+  }
+  function fim(c) {
+    window.alert('Terminou, venceu o jogador ' + c + ' ');
+    reinicia();
+  }
+  function jogada(elem, rodada) {
+    if (equals_0(elem.textContent, '#'))
+      if (rodada % 2 === 0)
+        elem.innerHTML = 'x';
+      else
+        elem.innerHTML = 'o';
+  }
+  function attRodada(rod, valor) {
+    rod.innerHTML = (valor + 1 | 0).toString();
+  }
+  function tabuleiroCheio() {
+    var tmp$;
+    var l = document.getElementsByTagName('td');
+    var tmp$_0;
+    tmp$_0 = asList(l).iterator();
+    while (tmp$_0.hasNext()) {
+      var element = tmp$_0.next();
+      if (equals_0(element.textContent, '#'))
+        return false;
+    }
+    tmp$ = asList(l).iterator();
+    while (tmp$.hasNext()) {
+      var e = tmp$.next();
+      println(e.textContent);
+    }
+    return true;
+  }
+  function checaLinhas$linha1Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5, tmp$_6, tmp$_7, tmp$_8, tmp$_9, tmp$_10, tmp$_11, tmp$_12;
+      var tmp$_13 = !equals_0((tmp$_0 = (tmp$ = closure$l[0]) != null ? tmp$.firstElementChild : null) != null ? tmp$_0.className : null, 'hashtag');
+      if (!tmp$_13) {
+        tmp$_13 = !equals_0((tmp$_2 = (tmp$_1 = closure$l[1]) != null ? tmp$_1.firstElementChild : null) != null ? tmp$_2.className : null, 'hashtag');
+      }
+      var tmp$_14 = tmp$_13;
+      if (!tmp$_14) {
+        tmp$_14 = !equals_0((tmp$_4 = (tmp$_3 = closure$l[2]) != null ? tmp$_3.firstElementChild : null) != null ? tmp$_4.className : null, 'hashtag');
+      }
+      if (tmp$_14) {
+        return equals_0((tmp$_6 = (tmp$_5 = closure$l[0]) != null ? tmp$_5.firstElementChild : null) != null ? tmp$_6.className : null, (tmp$_8 = (tmp$_7 = closure$l[1]) != null ? tmp$_7.firstElementChild : null) != null ? tmp$_8.className : null) ? equals_0((tmp$_10 = (tmp$_9 = closure$l[0]) != null ? tmp$_9.firstElementChild : null) != null ? tmp$_10.className : null, (tmp$_12 = (tmp$_11 = closure$l[2]) != null ? tmp$_11.firstElementChild : null) != null ? tmp$_12.className : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaLinhas$linha2Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
+      var tmp$_6 = !equals_0((tmp$ = closure$l[3]) != null ? tmp$.textContent : null, '#');
+      if (!tmp$_6) {
+        tmp$_6 = !equals_0((tmp$_0 = closure$l[4]) != null ? tmp$_0.textContent : null, '#');
+      }
+      var tmp$_7 = tmp$_6;
+      if (!tmp$_7) {
+        tmp$_7 = !equals_0((tmp$_1 = closure$l[5]) != null ? tmp$_1.textContent : null, '#');
+      }
+      if (tmp$_7) {
+        return equals_0((tmp$_2 = closure$l[3]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[4]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[3]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[5]) != null ? tmp$_5.textContent : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaLinhas$linha3Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
+      var tmp$_6 = !equals_0((tmp$ = closure$l[6]) != null ? tmp$.textContent : null, '#');
+      if (!tmp$_6) {
+        tmp$_6 = !equals_0((tmp$_0 = closure$l[7]) != null ? tmp$_0.textContent : null, '#');
+      }
+      var tmp$_7 = tmp$_6;
+      if (!tmp$_7) {
+        tmp$_7 = !equals_0((tmp$_1 = closure$l[8]) != null ? tmp$_1.textContent : null, '#');
+      }
+      if (tmp$_7) {
+        return equals_0((tmp$_2 = closure$l[6]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[7]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[6]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[8]) != null ? tmp$_5.textContent : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaLinhas() {
+    var l = document.getElementsByTagName('td');
+    var linha1Aux = checaLinhas$linha1Aux(l);
+    var linha2Aux = checaLinhas$linha2Aux(l);
+    var linha3Aux = checaLinhas$linha3Aux(l);
+    return linha1Aux();
+  }
+  function checaColuna$coluna1Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
+      var tmp$_6 = !equals_0((tmp$ = closure$l[0]) != null ? tmp$.textContent : null, '#');
+      if (!tmp$_6) {
+        tmp$_6 = !equals_0((tmp$_0 = closure$l[3]) != null ? tmp$_0.textContent : null, '#');
+      }
+      var tmp$_7 = tmp$_6;
+      if (!tmp$_7) {
+        tmp$_7 = !equals_0((tmp$_1 = closure$l[6]) != null ? tmp$_1.textContent : null, '#');
+      }
+      if (tmp$_7) {
+        return equals_0((tmp$_2 = closure$l[0]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[3]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[0]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[6]) != null ? tmp$_5.textContent : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaColuna$coluna2Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
+      var tmp$_6 = !equals_0((tmp$ = closure$l[1]) != null ? tmp$.textContent : null, '#');
+      if (!tmp$_6) {
+        tmp$_6 = !equals_0((tmp$_0 = closure$l[4]) != null ? tmp$_0.textContent : null, '#');
+      }
+      var tmp$_7 = tmp$_6;
+      if (!tmp$_7) {
+        tmp$_7 = !equals_0((tmp$_1 = closure$l[7]) != null ? tmp$_1.textContent : null, '#');
+      }
+      if (tmp$_7) {
+        return equals_0((tmp$_2 = closure$l[1]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[4]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[1]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[7]) != null ? tmp$_5.textContent : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaColuna$coluna3Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
+      var tmp$_6 = !equals_0((tmp$ = closure$l[2]) != null ? tmp$.textContent : null, '#');
+      if (!tmp$_6) {
+        tmp$_6 = !equals_0((tmp$_0 = closure$l[5]) != null ? tmp$_0.textContent : null, '#');
+      }
+      var tmp$_7 = tmp$_6;
+      if (!tmp$_7) {
+        tmp$_7 = !equals_0((tmp$_1 = closure$l[8]) != null ? tmp$_1.textContent : null, '#');
+      }
+      if (tmp$_7) {
+        return equals_0((tmp$_2 = closure$l[2]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[5]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[2]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[8]) != null ? tmp$_5.textContent : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaColuna() {
+    var l = document.getElementsByTagName('td');
+    var coluna1Aux = checaColuna$coluna1Aux(l);
+    var coluna2Aux = checaColuna$coluna2Aux(l);
+    var coluna3Aux = checaColuna$coluna3Aux(l);
+    return coluna1Aux() || coluna2Aux() || coluna3Aux();
+  }
+  function checaDiagonal$diagonal1Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
+      var tmp$_6 = !equals_0((tmp$ = closure$l[0]) != null ? tmp$.textContent : null, '#');
+      if (!tmp$_6) {
+        tmp$_6 = !equals_0((tmp$_0 = closure$l[4]) != null ? tmp$_0.textContent : null, '#');
+      }
+      var tmp$_7 = tmp$_6;
+      if (!tmp$_7) {
+        tmp$_7 = !equals_0((tmp$_1 = closure$l[8]) != null ? tmp$_1.textContent : null, '#');
+      }
+      if (tmp$_7) {
+        return equals_0((tmp$_2 = closure$l[0]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[4]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[0]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[8]) != null ? tmp$_5.textContent : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaDiagonal$diagonal2Aux(closure$l) {
+    return function () {
+      var tmp$, tmp$_0, tmp$_1, tmp$_2, tmp$_3, tmp$_4, tmp$_5;
+      var tmp$_6 = !equals_0((tmp$ = closure$l[6]) != null ? tmp$.textContent : null, '#');
+      if (!tmp$_6) {
+        tmp$_6 = !equals_0((tmp$_0 = closure$l[4]) != null ? tmp$_0.textContent : null, '#');
+      }
+      var tmp$_7 = tmp$_6;
+      if (!tmp$_7) {
+        tmp$_7 = !equals_0((tmp$_1 = closure$l[2]) != null ? tmp$_1.textContent : null, '#');
+      }
+      if (tmp$_7) {
+        return equals_0((tmp$_2 = closure$l[6]) != null ? tmp$_2.textContent : null, (tmp$_3 = closure$l[4]) != null ? tmp$_3.textContent : null) ? equals_0((tmp$_4 = closure$l[6]) != null ? tmp$_4.textContent : null, (tmp$_5 = closure$l[2]) != null ? tmp$_5.textContent : null) : false;
+      }
+       else
+        return false;
+    };
+  }
+  function checaDiagonal() {
+    var l = document.getElementsByTagName('td');
+    var diagonal1Aux = checaDiagonal$diagonal1Aux(l);
+    var diagonal2Aux = checaDiagonal$diagonal2Aux(l);
+    return diagonal1Aux() || diagonal2Aux();
+  }
+  function checa() {
+    return checaLinhas() || checaColuna() || checaDiagonal() || tabuleiroCheio();
+  }
+  function checaVitoria() {
+    return checaLinhas() || checaColuna() || checaDiagonal();
   }
   function main() {
-    var tmp$;
-    var img = Kotlin.isType(tmp$ = document.getElementById('imagem'), HTMLDivElement) ? tmp$ : throwCCE();
-    img.style.display = 'none';
   }
-  _.mostra = mostra;
-  _.get_61zpoe$ = get;
-  _.jogada = jogada;
+  _.clicaTeste = clicaTeste;
+  _.testeCheca = testeCheca;
+  _.update = update;
+  _.reinicia = reinicia;
+  _.fim_61zpoe$ = fim;
+  _.jogada_906ova$ = jogada;
+  _.attRodada_906ova$ = attRodada;
+  _.tabuleiroCheio = tabuleiroCheio;
+  _.checaLinhas = checaLinhas;
+  _.checaColuna = checaColuna;
+  _.checaDiagonal = checaDiagonal;
+  _.checa = checa;
+  _.checaVitoria = checaVitoria;
   _.main = main;
   main();
   Kotlin.defineModule('ProjetoLPF', _);
--- src/Velha.kt
+++ src/Velha.kt
@@ -1,44 +1,153 @@
-import kotlin.browser.*
-import kotlin.coroutines.EmptyCoroutineContext.get
 import org.w3c.dom.*
 import org.w3c.dom.events.MouseEvent
+import kotlin.browser.*
 
+@JsName("clicaTeste")
+fun clicaTeste(id: String){
+    val e = document.getElementById(id) as HTMLTableCellElement
+    if(e.firstElementChild!!.className.equals("hashtag")) e.innerHTML = "<img class=\"x\" src=\"img/x.jpg\"/>"
 
-
-fun mostra(){
-    val a = get("oi")
-    a.innerHTML += "pegou"
+    println(checaLinhas())
 }
 
-fun get(nome: String): HTMLElement{
-    val e = document.getElementById(nome)
-    return e as HTMLElement
+fun testeCheca(){
+    val l = document.getElementsByTagName("td")
+    println("pula")
+    println(l.get(0)?.firstElementChild?.className.equals("x"))
 }
 
-@JsName("jogada")
-fun jogada(pos: String){
-    val canv = document.getElementById(pos) as HTMLCanvasElement
-    val n = document.getElementById("numJogadas")
-    val valor: Int = if(n!=null)n.textContent!!.toInt() else 0
-    var img = document.getElementById("circulo") as HTMLImageElement
+@JsName("update")
+fun update(id: String): Unit{
+    if(!checa()) {
+        val e = document.getElementById(id) as HTMLTableCellElement
+        val rod = document.getElementById("rod") as HTMLElement
+        val valor: Int = rod.textContent!!.toInt()
 
-    if(valor % 2 == 0) {
-        img = document.getElementById("x") as HTMLImageElement
+
+        jogada(e, valor)
+        attRodada(rod, valor)//atualiza o valor da rodada
+
+        if(checaVitoria()) if(valor%2==0) fim("x") else fim("o")
+    } else
+        if(tabuleiroCheio()) window.alert("Empate!")
+
+}
+
+@JsName("reinicia")
+fun reinicia(): Unit{
+    val e = document.getElementsByTagName("td")
+    e.asList().forEach { obj ->
+        obj.innerHTML = "#"
+    }
+    document.getElementById("rod")?.innerHTML = 0.toString()
+}
+
+fun fim(c: String): Unit {
+    window.alert("Terminou, venceu o jogador $c ")
+    reinicia()
+}
+
+fun jogada(elem: HTMLElement, rodada: Int): Unit{ //faz um movimento no html ao clicar numa celula vazia
+    if (elem.textContent.equals("#"))
+        if (rodada % 2 == 0)
+            elem.innerHTML = "x"
+        else
+            elem.innerHTML = "o"
+}
+
+fun attRodada(rod: HTMLElement, valor: Int): Unit{
+    rod.innerHTML= (valor+1).toString()
+}
+
+fun tabuleiroCheio(): Boolean{
+    val l = document.getElementsByTagName("td")
+    l.asList().forEach { obj ->
+        if(obj.textContent.equals("#")) return false
+    }
+    for(e in l.asList())
+        println(e.textContent)
+    return true
+}
+
+fun checaLinhas(): Boolean{
+    val l = document.getElementsByTagName("td")
+
+    fun linha1Aux(): Boolean { //checa linha 1
+        if(!l.get(0)?.firstElementChild?.className.equals("hashtag") || !l.get(1)?.firstElementChild?.className.equals("hashtag")
+                || !l.get(2)?.firstElementChild?.className.equals("hashtag"))
+            return (l.get(0)?.firstElementChild?.className.equals(l.get(1)?.firstElementChild?.className) && l.get(0)?.firstElementChild?.className.equals(l.get(2)?.firstElementChild?.className))
+        else
+            return false
     }
 
-    val context = canv.getContext("2d") as CanvasRenderingContext2D
+    fun linha2Aux(): Boolean { //checa linha 2
+        if(!l.get(3)?.textContent.equals("#") || !l.get(4)?.textContent.equals("#") || !l.get(5)?.textContent.equals("#"))
+            return (l.get(3)?.textContent.equals(l.get(4)?.textContent) && l.get(3)?.textContent.equals(l.get(5)?.textContent))
+        else
+            return false
+    }
 
-    //if(context == null) {
-    context.drawImage(img, 0.0, 0.0, 100.0, 100.0)
+    fun linha3Aux(): Boolean { //checa linha 3
+        if(!l.get(6)?.textContent.equals("#") || !l.get(7)?.textContent.equals("#") || !l.get(8)?.textContent.equals("#"))
+            return (l.get(6)?.textContent.equals(l.get(7)?.textContent) && l.get(6)?.textContent.equals(l.get(8)?.textContent))
+        else
+            return false
+    }
 
-    n?.innerHTML = (valor + 1).toString()
-    //}
+    return linha1Aux()
 }
 
+fun checaColuna(): Boolean{
+    val l = document.getElementsByTagName("td")
 
+    fun coluna1Aux(): Boolean{
+        if(!l.get(0)?.textContent.equals("#") || !l.get(3)?.textContent.equals("#") || !l.get(6)?.textContent.equals("#"))
+            return (l.get(0)?.textContent.equals(l.get(3)?.textContent) && l.get(0)?.textContent.equals(l.get(6)?.textContent))
+        else
+            return false
+    }
 
+    fun coluna2Aux(): Boolean { //checa linha 2
+        if(!l.get(1)?.textContent.equals("#") || !l.get(4)?.textContent.equals("#") || !l.get(7)?.textContent.equals("#"))
+            return (l.get(1)?.textContent.equals(l.get(4)?.textContent) && l.get(1)?.textContent.equals(l.get(7)?.textContent))
+        else
+            return false
+    }
 
-fun main(){
-    val img = document.getElementById("imagem") as HTMLDivElement
-    img.style.display = "none"
+    fun coluna3Aux(): Boolean { //checa linha 2
+        if(!l.get(2)?.textContent.equals("#") || !l.get(5)?.textContent.equals("#") || !l.get(8)?.textContent.equals("#"))
+            return (l.get(2)?.textContent.equals(l.get(5)?.textContent) && l.get(2)?.textContent.equals(l.get(8)?.textContent))
+        else
+            return false
+    }
+
+    return coluna1Aux() || coluna2Aux() || coluna3Aux()
+}
+
+fun checaDiagonal(): Boolean{
+    val l = document.getElementsByTagName("td")
+
+    fun diagonal1Aux(): Boolean{
+        if(!l.get(0)?.textContent.equals("#") || !l.get(4)?.textContent.equals("#") || !l.get(8)?.textContent.equals("#"))
+            return (l.get(0)?.textContent.equals(l.get(4)?.textContent) && l.get(0)?.textContent.equals(l.get(8)?.textContent))
+        else
+            return false
+    }
+
+    fun diagonal2Aux(): Boolean{
+        if(!l.get(6)?.textContent.equals("#") || !l.get(4)?.textContent.equals("#") || !l.get(2)?.textContent.equals("#"))
+            return (l.get(6)?.textContent.equals(l.get(4)?.textContent) && l.get(6)?.textContent.equals(l.get(2)?.textContent))
+        else
+            return false
+    }
+
+    return diagonal1Aux() || diagonal2Aux()
+}
+
+fun checa(): Boolean = checaLinhas() || checaColuna() || checaDiagonal() || tabuleiroCheio()
+
+fun checaVitoria() = checaLinhas() || checaColuna() || checaDiagonal()
+
+fun main() {
+
 }
